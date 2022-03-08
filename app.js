1a7fca9b5f81afb24a5b61bba06ccc9147c956a0
SPC Ezz <16054551+sara2in@users.noreply.github.com>
2022-03-08 13:03:06 -0500

    app.js fix

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// app.js
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const knex = require('knex')(require('path/to/knexfile.js')[process.env.NODE_ENV]);
+const knex = require("knex")(
+  require("./knexfile.js")[process.env.NODE_ENV || "development"]
+);
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-app.get('/movies', function(req, res) {
+app.get("/", function (req, res) {
   knex
-    .select('*')
-    .from('movies')
-    .then(data => res.status(200).json(data))
-    .catch(err =>
+    .select("*")
+    .from("movies")
+    .then((data) => res.status(200).json(data))
+    .catch((err) =>
       res.status(404).json({
         message:
-          'The data you are looking for could not be found. Please try again'
+          "The data you are looking for could not be found. Please try again",
       })
     );
 });
